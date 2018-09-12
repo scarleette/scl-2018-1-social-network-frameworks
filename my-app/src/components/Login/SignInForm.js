b089b01ccec1ebb3c904c6a26d4c88753f990e14
Camila <camipereira2111@gmail.com>
2018-09-11 21:21:47 -0300

    estado de boton Login

--- my-app/src/components/Login/SignInForm.js
+++ my-app/src/components/Login/SignInForm.js
@@ -4,15 +4,26 @@ import Registro from '../registro/Registro';
 class SignInForm extends Component{
     constructor(){
         super();
+
+        this.state = {
+            status:''
+        }
     }
     render(){
-        return
+        return(
         <div>
             <input type="text"/>
             <input type="password"/>
-            <button id='loginBtn'onClick={Registro}>Login</button>
+            <button id='loginBtn'onClick={this.firebaseLogin.bind(this)}>Login</button>
             <button id='fbLogBtn' onClick={facebookLogin()}>Conectate con Facebook</button>
         </div>
+        )
+    }
+    firebaseLogin(){
+        console.log(this.state);
+        
     }
 }
+
+
 export default SignInForm
