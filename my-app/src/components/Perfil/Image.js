4ad2765ded2e032e397d4bbcf03c41325af9f3cd
Camila <camipereira2111@gmail.com>
2018-09-14 20:00:21 -0300

    reemplazo por Materialize la img anterior

--- my-app/src/components/Perfil/Image.js
+++ my-app/src/components/Perfil/Image.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Image = ()=> {
     return(
         <div>
-            <img src="/img/profile.jpeg" alt="" srcset=""/>
+            <Card className='small' header={<CardTitle image='img/sample-1.jpg'>User Name</CardTitle>}>
+            </Card>
         </div>
         )
     };
