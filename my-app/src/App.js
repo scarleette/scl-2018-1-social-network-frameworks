a16545b161e203b1ca7eb66cd444b9483de1c3e5
scarleette <scar.antoine@gmail.com>
2018-09-20 19:16:17 -0300

    Incorporacion de firebase

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,18 @@ import Registro from './components/registro/Registro';
 import VistaMuro from './components/Muro/VistaMuro';
 // import Login from '../src/components/Login/Login';
 import Perfil from '../src/components/Perfil/Perfil';
+import firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAXTZOPfhB0mKN47rxIuMs9H_mc8odWS5g",
+  authDomain: "red-social-framework.firebaseapp.com",
+  databaseURL: "https://red-social-framework.firebaseio.com",
+  projectId: "red-social-framework",
+  storageBucket: "red-social-framework.appspot.com",
+  messagingSenderId: "359981357151"
+};
+firebase.initializeApp(config);
 
 
 class App extends Component {
