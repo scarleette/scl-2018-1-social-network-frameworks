777b61788014b0e0b664a612375a8ec1261bb156
Camila <camipereira2111@gmail.com>
2018-09-20 20:11:23 -0300

    cambios en app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import * as firebase from 'firebase';
 import './App.css';
 import Login from '../src/components/Login/Login';
 import Perfil from '../src/components/Perfil/Perfil';
