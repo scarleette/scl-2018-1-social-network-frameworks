b292186c31b4cc0e99ce0e5698bad02737182cba
Camila <camipereira2111@gmail.com>
2018-09-22 16:34:52 -0300

    arreglos Firebase

--- my-app/src/components/Login/SignInForm.js
+++ my-app/src/components/Login/SignInForm.js
@@ -1,34 +1,39 @@
 import React, { Component } from 'react';
 import {Row, Input, Button, Icon} from 'react-materialize';
-import firebase from 'firebase';
+import firebase from '../../firebase/firebase';
 
 class SignInForm extends Component{
 	constructor(){
-		super()
+		super();
+
 		this.state = {
 			email : '',
 			password : ''
 		}
+	
+		firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+		.then( user => {
+
+			Location.href = '../perfil/perfil.js';
+
+		})
+		.catch(function(error) {
+
+			console.log(error);
+		});
+		
 	}
 	render(){
 		return(
-			<Row>
-				<Input type="email" label="Email" s={12} />
-				<Input type="password" label="Password" s={12} />
-				<Button waves='light'>Log In <Icon left>send</Icon></Button>
-			</Row>
-		
-	)
-	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-		// Handle Errors here.
-		var errorCode = error.code;
-		var errorMessage = error.message;
-		// ...
-	});
+			<div>
+				<Row>
+					<Input type="email" label="Email" s={12} value = {this.state.email} />
+					<Input type="password" label="Password" s={12} value = {this.state.password} />
+					<Button waves='light'>Log In <Icon left>send</Icon></Button>
+				</Row>
+			</div>
+		)
 	}
-	
 }
-
-
 
 export default SignInForm;
