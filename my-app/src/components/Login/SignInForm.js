2b9eba817f23e45e7a7fb20f937496fc53ef2a5d
Camila <camipereira2111@gmail.com>
2018-09-23 18:24:15 -0300

    componente SiginForm con funcion de autentificacion de usuario

--- my-app/src/components/Login/SignInForm.js
+++ my-app/src/components/Login/SignInForm.js
@@ -1,39 +1,55 @@
 import React, { Component } from 'react';
-import {Row, Input, Button, Icon, Col} from 'react-materialize';
-import firebase from 'firebase';
+import {Button, Icon, Input, Row, Col} from 'react-materialize';
+import firebase from '../../firebase/firebase';
 
-class SignInForm extends Component{
-	constructor(props){
-		super(props);
 
-		this.state = {
-			email: '',
-			password: ''
-		}	
+class SignInForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      
+      email: '',
+      password: ''
+
+    }
+
+  this.handleChange = this.handleChange.bind(this);
+  this.usuarioLogueado = this.usuarioLogueado.bind(this);
+
+  }
+ 
+  render() {
+    return (
+      <div>
+        <Row>
+            <Input s={12} value={this.state.email} onChange={this.handleChange} type="email" name="email" placeholder="Email"><Icon>account_circle</Icon></Input>
+            <Input s={12} value={this.state.password} onChange={this.handleChange} type="password" name="password"  placeholder="Password"><Icon >lock</Icon></Input>
+        </Row>
+        
+          <div className = "button">
+            <center><Button waves='light' onClick={this.usuarioLogueado}><Icon left>send</Icon>Log In </Button></center>
+          </div>
+      </div>
+    )
+  }
+  usuarioLogueado(event) {
+    event.preventDefault();
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+		.then((user)=>
+			
+			{console.log(user)})
+
+    .catch((error) => {
+			
+        console.log(error);
+      })
 	}
 
-	render(){
-		return(
-			<div>
-				<Row>
-				<Input s={12} value={this.state.email} onChange={this.handleChange.bind(this)} type="email" name="email" placeholder="Email"><Icon>account_circle</Icon></Input>
-            <Input s={12} value={this.state.password} onChange={this.handleChange.bind(this)} type="password" name="password"  placeholder="Password"><Icon >lock</Icon></Input>
-					<Button waves='light' onClick = {this.cambioVista}>Log In <Icon left>send</Icon></Button>
-				</Row>
-			</div>
-		)
-	}
-	handleChange(e) {
-		e.preventDefault();
-		this.setState({ [e.target.name]: e.target.value });
-		firebase.auth().signInWithEmailAndPassword(e.state.email, e.state.password)
-		.then(result => {
-			console.log('hola estas regustrado' + result);
-		})
-		.catch(function(error) {
-			console.log(error);
-		});
-	}
-}
+  		handleChange(e) {
+  		this.setState({ [e.target.name]: e.target.value });
+  }
+};
 
-export default SignInForm;
+export default SignInForm ;
+
