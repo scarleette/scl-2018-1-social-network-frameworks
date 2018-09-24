2c2d721ae198af9cbe788ad5c5c26a21c1098f13
scarleette <scar.antoine@gmail.com>
2018-09-24 01:21:06 -0300

    Cambio de foto para Carrusel

--- my-app/src/components/Common/Carrusel.js
+++ my-app/src/components/Common/Carrusel.js
@@ -4,8 +4,8 @@ import {Carousel} from 'react-materialize';
 const Carrusel = () => {
 	return(
 		<Carousel options={{ fullWidth: true }} images={[
-			"https://www.losreplicantes.com/images/articulos/1000/1899/top.jpg",
-			'https://lorempixel.com/800/400/food/3',
+			"https://sites.google.com/site/veterinarialasmascotas/_/rsrc/1453351446585/home/bienvenida.jpg",
+			'https://zoovetesmipasion.com/wp-content/uploads/2017/04/veterinario.png',
 			'https://lorempixel.com/800/400/food/4',
 			'https://lorempixel.com/800/400/food/5'
 		]} />
