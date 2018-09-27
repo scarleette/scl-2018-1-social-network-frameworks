679318f6b4a7b528578d660ee8517ef45b81a439
scarleette <scar.antoine@gmail.com>
2018-09-27 10:18:29 -0300

    hoja de estilo para Login

--- my-app/src/components/Login/Login.js
+++ my-app/src/components/Login/Login.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Titulo from '../registro/Titulo';
 import Registro from '../registro/Registro';
 import SignInForm from './SignInForm';
-import LoginGoogle from './LoginGoogle';
+
+import './Login.css';
 
 
 
@@ -15,7 +16,6 @@ const Login = () =>{
 			<div className = "containerLogin">
 				<Titulo titulo="Little Pet Society" />
 				<SignInForm />
-				<LoginGoogle/>
 				<Link to="/registro">Registrate con Nosotros</Link>
 				<Route path="/registro" component={Registro}></Route>   
 			</div>
--- my-app/src/components/Login/SignInForm.js
+++ my-app/src/components/Login/SignInForm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {Redirect,withRouter} from 'react-router-dom';
-import {Button, Icon, Input, Row} from 'react-materialize';
+import {Button, Icon, Input, Row, Col} from 'react-materialize';
 import firebase from '../../firebase/firebase';
+import LoginGoogle from './LoginGoogle';
 import Perfil from '../Perfil/Perfil';
 import Login from './Login';
 
@@ -41,13 +42,15 @@ usuarioLogueado(event) {
     return (
       <div>
         <Row>
-            <Input s={12} value={this.state.email} onChange={this.handleChange} type="email" name="email" placeholder="Email"><Icon>account_circle</Icon></Input>
-            <Input s={12} value={this.state.password} onChange={this.handleChange} type="password" name="password"  placeholder="Password"><Icon >lock</Icon></Input>
+          <Input Col s={12} value={this.state.email} onChange={this.handleChange} type="email" name="email" placeholder="Email"><Icon>account_circle</Icon></Input>
+          <Input Col s={12} value={this.state.password} onChange={this.handleChange} type="password" name="password"  placeholder="Password"><Icon >lock</Icon></Input>
         </Row>
-        
-          <div className = "button">
-            <center><Button waves='light' onClick={this.usuarioLogueado}><Icon left>send</Icon>Log In </Button></center>
-          </div>
+        <div Col s = {6}>
+          <LoginGoogle/>
+        </div>
+        <div Col s = {6}>
+          <Button waves='light' onClick={this.usuarioLogueado}><Icon left>send</Icon>Log In </Button>
+        </div>
       </div>
     )
   }
