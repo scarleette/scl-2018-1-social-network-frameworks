662c93afd8cf38db3beb59c1bdbda48c2c3b1db5
scarleette <scar.antoine@gmail.com>
2018-09-27 11:59:37 -0300

    Cambio de sintaxis para rutas de navbar

--- my-app/src/components/Common/Navbar.js
+++ my-app/src/components/Common/Navbar.js
@@ -7,11 +7,11 @@ const NavBar = () => {
         <Navbar brand='Little Pet Society' right>
           {/* <NavItem onClick={() => console.log('test click')}>Getting started</NavItem*/}
           <NavItem divider />
-          <NavItem href='../Perfil/Perfil.js'><Icon>perm_identity</Icon></NavItem>
-          <NavItem href=''><Icon>help_outline</Icon></NavItem>
-          <NavItem href='../Muro/VistaMuro.js'><Icon>chrome_reader_mode</Icon></NavItem>
-          <NavItem href=''><Icon>chat</Icon></NavItem>
-          <NavItem href=''><Icon>close</Icon></NavItem>
+          <NavItem href={process.env.PUBLIC_URL + '/Perfil'}><Icon>perm_identity</Icon></NavItem>
+          <NavItem href={process.env.PUBLIC_URL + '/'}><Icon>help_outline</Icon></NavItem>
+          <NavItem href={process.env.PUBLIC_URL + '/VistaMuro'}><Icon>chrome_reader_mode</Icon></NavItem>
+          <NavItem href={process.env.PUBLIC_URL + '/'}><Icon>chat</Icon></NavItem>
+          <NavItem href={process.env.PUBLIC_URL + '/'}><Icon>close</Icon></NavItem>
         </Navbar>
       </div>
     )
